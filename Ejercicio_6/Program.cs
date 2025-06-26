f06ffe32c62ed138565728b5d94754089b829046
Gemino <71484444@idexperujapon.edu.pe>
2025-06-26 17:23:39 -0500

    finalizado

--- Ejercicio_6/Program.cs
+++ Ejercicio_6/Program.cs
@@ -10,9 +10,13 @@ namespace Ejercicio_6
     {
         static void Main(string[] args)
         {
+            //declaramos variables
             int n1;
+            //pedimos el numero en pantalla
             Console.WriteLine("Introduce un número entero: ");
+            //ingresar el número con el teclado
             n1 = Convert.ToInt32(Console.ReadLine());
+            //esta es la variante si que se ejecuta si se cumple una condición
             if (n1 > 0)
             {
                 Console.WriteLine("El número es positivo");
